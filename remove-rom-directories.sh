03f797b9ff89fb7e6f0a8958fbbb1e747b7c6d7f
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2015-01-07 15:42:54 +0100

    Integrate cm-update-server into the "remove-rom-directories" script.

--- remove-rom-directories.sh
+++ remove-rom-directories.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ROM_NAME="${1}"
+DEVICE_ID="${2}"
 
 if [ -z "${ROM_NAME}" ]
 then
@@ -14,6 +15,31 @@ then
 	exit 0
 fi
 
+for FILE in $(find "${BIN_DIR}/roms/${ROM_NAME}" -type f -print)
+do
+	echo "Removing '${FILE}'..."
+
+	if [[ -n "${ROM_DATABASE_SCRIPT_DIR}" && $FILE =~ \.zip$ ]]
+	then
+		FILENAME=$(basename $FILE)
+
+		if [ -z "${DEVICE_ID}" ]
+		then
+			echo "ERROR: $0 requires argument 2 (DEVICE_ID) to disable existing ROMs"
+			exit 1
+		fi
+
+		(cd $ROM_DATABASE_SCRIPT_DIR && \
+			node disable-build.js \
+				--device $DEVICE_ID \
+				--filename $FILENAME \
+				--subdirectory $ROM_NAME \
+				--disable_incrementals)
+	fi
+
+	rm $FILE
+done
+
+rmdir "${BIN_DIR}/roms/${ROM_NAME}"
 rm -rf "${BIN_DIR}/targetfiles/${ROM_NAME}"
-rm -rf "${BIN_DIR}/roms/${ROM_NAME}"
 rm -rf "${BIN_DIR}/incrementals/${ROM_NAME}"
